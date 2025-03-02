20b6e1c3e5b75fe3bfd65dc49697a050ac5b0d39
Kaima Oka <kaima0303oka@icloud.com>
2025-03-03 05:00:56 +0900

    Next.js設定を更新し、出力ディレクトリを'export'に変更。トレーリングスラッシュとdistDirの設定を削除。

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "export",
 	images: {
 		unoptimized: true,
 	},
 	compiler: {
 		styledComponents: true,
 	},
-	trailingSlash: true,
-	distDir: "out",
 };
 
 export default nextConfig;
