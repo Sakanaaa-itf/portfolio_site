d703ad6ecf034aecd08fa802497b59b4e20bc4bb
Kaima Oka <kaima0303oka@icloud.com>
2025-03-02 16:52:53 +0900

    next.config.tsに出力設定と画像の最適化無効化を追加し、wrangler.tomlにアプリ名とアカウント情報を設定

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
   compiler: {
     styledComponents: true,
   },
