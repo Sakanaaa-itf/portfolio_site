cf02fd464a37b679acd3fd3914cc3ff776ced887
Kaima Oka <kaima0303oka@icloud.com>
2025-03-13 19:21:36 +0900

    ｵｲｼｮ! \ｵｲｼｮ!/ ｵｲｼｮ! \ｵｲｼｮ!/ ｵｲｼｮ! \ｵｲｼｮ!/ ｵｲｼｮ! \ｵｲｼｮ!/

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -60,8 +60,8 @@ export const photos: PhotoMeta[] = [
 			"https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/E9F54951-2A8F-4D1C-801D-090C3957E512-5334-00000216DF3D70C3_low.webp",
 		highResUrl:
 			"https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/E9F54951-2A8F-4D1C-801D-090C3957E512-5334-00000216DF3D70C3.webp",
-		title: "奄美大島にて",
-		comment: "湾と市街を海側から見渡せる場所。",
+		title: "名瀬港",
+		comment: "湾と市街を海側から見渡せる場所から撮影。",
 		uploadedAt: "2023-09-14",
 	},
 	{
@@ -93,4 +93,76 @@ export const photos: PhotoMeta[] = [
 		comment: "実験中の様子。綺麗に撮れたけど地獄でした。",
 		uploadedAt: "2023-07-19",
 	},
+	{
+		id: "photo10",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/7C4DF802-4D05-4B74-9F3A-BF617E09F0AE-52388-00001D904780C86C_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/7C4DF802-4D05-4B74-9F3A-BF617E09F0AE-52388-00001D904780C86C.webp",
+		title: "涸沼",
+		comment: "大洗に行く途中にある涸沼駅にて。気動車が走っていて感動。フォントがいいなーなんて思っていたんですけど、名前はわかりません。",
+		uploadedAt: "2024-01-27"
+	},
+	{
+		id: "photo11",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF0016_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF0016.webp",
+		title: "花火1",
+		comment: "よくわからない河川敷にて。写っている手の人には長らく会っていません。元気なんすかね。",
+		uploadedAt: "2023-08-12"
+	},
+	{
+		id: "photo12",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF0275_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF0275.webp",
+		title: "平面",
+		comment: "自分はあまり撮影しない平面の写真。緑のトンネルをとった直後にこれを見つけて、撮影しました。",
+		uploadedAt: "2023-08-25"
+	},
+	{
+		id: "photo13",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF0773%201_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF0773%201.webp",
+		title: "里の曙",
+		comment: "帰省した時に飲んだ、黒糖焼酎。",
+		uploadedAt: "2023-09-08"
+	},
+	{
+		id: "photo14",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF0813_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF0813.webp",
+		title: "花火2",
+		comment: "地元では有名な場所である、通称三角浜にて。",
+		uploadedAt: "2023-09-09"
+	},
+	{
+		id: "photo15",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF2790_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF2790.webp",
+		title: "鉄塔と星",
+		comment: "星空の下、鉄塔を撮影。長秒露光して星だけを動かした。",
+		uploadedAt: "2023-10-22"
+	},
+	{
+		id: "photo16",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF8063_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF8063.webp",
+		title: "海と友人と",
+		comment: "カメラを買って初めて人物を主題に撮影した写真。誘ってくれた友人とモデルになってくれた友人に感謝。",
+		uploadedAt: "2023-07-06"
+	},
+	{
+		id: "photo17",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF8674_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF8674.webp",
+		title: "流しマカロニ",
+		comment: "筑波大学内にある石の広場にて、友人が企画してくれた流しそうめん。流すものがなくなって、自分が到着したころには流しマカロニになっていました。",
+		uploadedAt: "2023-07-12"
+	},
+	{
+		id: "photo18",
+		lowResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%82%8D%E3%83%BC%E3%82%8C%E3%81%9D%E3%82%99/DSCF9639_low.webp",
+		highResUrl: "https://xn--l8j8cqftsc.xn--19ja1fb.xn--q9jyb4c/%E3%81%AF%E3%81%84%E3%82%8C%E3%81%9D%E3%82%99/DSCF9639.webp",
+		title: "now on stage",
+		comment: "成人式のため帰省した時の、初のライブハウスにて。友人達のバンドが出演していたので、それを撮影。カッコ良すぎる。",
+		uploadedAt: "2024-01-05"
+	}
 ];
