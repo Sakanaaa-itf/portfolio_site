2f05b626369e628832ed77c1e3aa95fa261378d7
Kaima Oka <kaima0303oka@icloud.com>
2025-04-17 17:19:14 +0900

    OK

--- src/components/CommentExif.tsx
+++ src/components/CommentExif.tsx
@@ -21,7 +21,7 @@ const CommentBox = styled.div`
 
 const ExifBox = styled.div<{ $isMobile: boolean }>`
 	display: flex;
-	flex-direction: ${({ $isMobile }) => ($isMobile ? "column" : "row")}; /* ✨ */
+	flex-direction: ${({ $isMobile }) => ($isMobile ? "column" : "row")};
 	gap: 1.5rem;
 	font-size: 12px;
 	line-height: 1.4;
@@ -43,6 +43,20 @@ const ExifLine = styled.p`
 
 const LinkText = styled.a`
 	color: #1da1f2;
+	&:hover {
+		opacity: 0.8;
+	}
+`;
+
+const ShareBtn = styled.a`
+	display: inline-block;
+	margin-top: 1.5rem;
+	padding: 0.5rem 1rem;
+	font-size: 12px;
+	background-color: #1da1f2;
+	color: #fff;
+	border-radius: 4px;
+	text-decoration: none;
 	&:hover {
 		opacity: 0.8;
 	}
@@ -76,12 +90,19 @@ export default function CommentExif({ photo }: { photo: PhotoMeta }) {
 				const data = await getExifDataForPhoto(photo);
 				if (!cancelled) setExif(data);
 			} catch {
+				// ignore
 			}
 		})();
 		return () => {
 			cancelled = true;
 		};
 	}, [photo]);
+
+	const shareUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+		`Check out this photo: "${photo.title}" \n`
+	)}&url=${encodeURIComponent(
+		`https://xn--19ja1fb.xn--q9jyb4c/photoworks/${photo.id}`
+	)}`;
 
 	return (
 		<Wrapper $isMobile={isMobile}>
@@ -97,9 +118,15 @@ export default function CommentExif({ photo }: { photo: PhotoMeta }) {
 					<ExifLine>Aperture&nbsp;:&nbsp;{exif?.aperture || "-"}</ExifLine>
 					<ExifLine>SS&nbsp;:&nbsp;{exif?.shutterSpeed || "-"}</ExifLine>
 					<ExifLine>ISO&nbsp;:&nbsp;{exif?.iso || "-"}</ExifLine>
-					<ExifLine>Focal&nbsp;Length&nbsp;:&nbsp;{exif?.focalLength || "-"}</ExifLine>
+					<ExifLine>
+						Focal&nbsp;Length&nbsp;:&nbsp;{exif?.focalLength || "-"}
+					</ExifLine>
 				</ExifColumn>
 			</ExifBox>
+
+			<ShareBtn href={shareUrl} target="_blank" rel="noopener noreferrer">
+				share to Twitter
+			</ShareBtn>
 		</Wrapper>
 	);
 }
