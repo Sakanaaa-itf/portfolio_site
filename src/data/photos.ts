57bacee25fecbd38369759b94f5eab2e9b8e8b89
Kaima Oka <kaima0303oka@icloud.com>
2025-04-17 13:10:04 +0900

    ｵｲｼｮ! \ｵｲｼｮ!/ ｵｲｼｮ! \ｵｲｼｮ!/ ｵｲｼｮ! \ｵｲｼｮ!/ ｵｲｼｮ! \ｵｲｼｮ!/

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -32,7 +32,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	{
 		id: "photo1",
 		lowResUrl:
-			"DSCF0546_400.webp",
+			"DSCF0546_1600.webp",
 		highResUrl: "DSCF0546_3200.jpg",
 		title: "着陸前",
 		comment:
@@ -42,7 +42,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	{
 		id: "photo2",
 		lowResUrl:
-			"DSCF0276_400.webp",
+			"DSCF0276_1600.webp",
 		highResUrl: "DSCF0276_3200.jpg",
 		title: "夏のはじまる前、散歩中に。",
 		comment: "緑のトンネル。真ん中に被写体をおきたかった…",
@@ -51,7 +51,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	{
 		id: "photo3",
 		lowResUrl:
-			"DSCF7826_400.webp",
+			"DSCF7826_1600.webp",
 		highResUrl: "DSCF7826_3200.jpg",
 		title: "Dodge Charenger R/T 440 six pack",
 		comment: "友人の車。アメ車の中では一番好き。",
@@ -60,7 +60,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	{
 		id: "photo4",
 		lowResUrl:
-			"_DSF7152_400.webp",
+			"_DSF7152_1600.webp",
 		highResUrl: "_DSF7152_3200.jpg",
 		title: "やどかり祭にて",
 		comment: "逆光ってえっちですよね。",
@@ -69,7 +69,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	{
 		id: "photo5",
 		lowResUrl:
-			"DSCF0845_400.webp",
+			"DSCF0845_1600.webp",
 		highResUrl: "DSCF0845_3200.jpg",
 		title: "夏の終わりに。",
 		comment: "地元の海沿いに生えている、アダンという木。",
@@ -78,7 +78,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	{
 		id: "photo6",
 		lowResUrl:
-			"E9F54951-2A8F-4D1C-801D-090C3957E512-5334-00000216DF3D70C3_400.webp",
+			"E9F54951-2A8F-4D1C-801D-090C3957E512-5334-00000216DF3D70C3_1600.webp",
 		highResUrl: "E9F54951-2A8F-4D1C-801D-090C3957E512-5334-00000216DF3D70C3_3200.jpg",
 		title: "名瀬港",
 		comment: "湾と市街を海側から見渡せる場所から撮影。",
@@ -106,7 +106,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	{
 		id: "photo9",
 		lowResUrl:
-			"D081D1C8-C1BD-4F72-8504-0D498A495537-19829-000004ACF9D55EE1_400.webp",
+			"D081D1C8-C1BD-4F72-8504-0D498A495537-19829-000004ACF9D55EE1_1600.webp",
 		highResUrl: "D081D1C8-C1BD-4F72-8504-0D498A495537-19829-000004ACF9D55EE1_3200.jpg",
 		title: "実験中",
 		comment: "実験中の様子。綺麗に撮れたけど地獄でした。",
@@ -114,7 +114,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo10",
-		lowResUrl: "7C4DF802-4D05-4B74-9F3A-BF617E09F0AE-52388-00001D904780C86C_400.webp",
+		lowResUrl: "7C4DF802-4D05-4B74-9F3A-BF617E09F0AE-52388-00001D904780C86C_1600.webp",
 		highResUrl: "7C4DF802-4D05-4B74-9F3A-BF617E09F0AE-52388-00001D904780C86C_3200.jpg",
 		title: "涸沼",
 		comment: "大洗に行く途中にある涸沼駅にて。気動車が走っていて感動。フォントがいいなーなんて思っていたんですけど、名前はわかりません。",
@@ -122,7 +122,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo11",
-		lowResUrl: "DSCF0016_400.webp",
+		lowResUrl: "DSCF0016_1600.webp",
 		highResUrl: "DSCF0016_3200.jpg",
 		title: "花火1",
 		comment: "よくわからない河川敷にて。写っている手の人には長らく会っていません。元気なんすかね。",
@@ -130,7 +130,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo12",
-		lowResUrl: "DSCF0275_400.webp",
+		lowResUrl: "DSCF0275_1600.webp",
 		highResUrl: "DSCF0275_3200.jpg",
 		title: "平面",
 		comment: "自分はあまり撮影しない平面の写真。緑のトンネルをとった直後にこれを見つけて、撮影しました。",
@@ -138,7 +138,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo13",
-		lowResUrl: "DSCF0773%201_400.webp",
+		lowResUrl: "DSCF0773%201_1600.webp",
 		highResUrl: "DSCF0773%201_3200.jpg",
 		title: "里の曙",
 		comment: "帰省した時に飲んだ、黒糖焼酎。全然別の銘柄のグラスに注がれていますね。",
@@ -146,7 +146,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo14",
-		lowResUrl: "DSCF0813_400.webp",
+		lowResUrl: "DSCF0813_1600.webp",
 		highResUrl: "DSCF0813_3200.jpg",
 		title: "花火2",
 		comment: "地元では有名な場所である、通称三角浜にて。",
@@ -154,7 +154,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo15",
-		lowResUrl: "DSCF2790_400.webp",
+		lowResUrl: "DSCF2790_1600.webp",
 		highResUrl: "DSCF2790_3200.jpg",
 		title: "鉄塔と星",
 		comment: "星空の下、鉄塔を撮影。長秒露光して星だけを動かした。車のハザードを焚きながら待っていたら、故障車だと思われて警察に声をかけられてしまった。",
@@ -162,7 +162,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo16",
-		lowResUrl: "DSCF8063_400.webp",
+		lowResUrl: "DSCF8063_1600.webp",
 		highResUrl: "DSCF8063_3200.jpg",
 		title: "海と友人と",
 		comment: "カメラを買って初めて人物を主題に撮影した写真。誘ってくれた友人とモデルになってくれた友人に感謝。",
@@ -170,7 +170,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo17",
-		lowResUrl: "DSCF8674_400.webp",
+		lowResUrl: "DSCF8674_1600.webp",
 		highResUrl: "DSCF8674_3200.jpg",
 		title: "流しマカロニ",
 		comment: "筑波大学内にある石の広場にて、友人が企画してくれた流しそうめん。流すものがなくなって、自分が到着したころには流しマカロニになっていました。",
@@ -178,7 +178,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo18",
-		lowResUrl: "DSCF9639_400.webp",
+		lowResUrl: "DSCF9639_1600.webp",
 		highResUrl: "DSCF9639_3200.jpg",
 		title: "now on stage",
 		comment: "成人式のため帰省した時のライブハウスにて。友人達のバンドが出演していたので、それを撮影。カッコ良すぎる。",
@@ -194,7 +194,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo20",
-		lowResUrl: "_DSF0382-強化-NR_400.webp",
+		lowResUrl: "_DSF0382-強化-NR_1600.webp",
 		highResUrl: "_DSF0382-強化-NR_3200.jpg",
 		title: "呪って呪って",
 		comment: "Bombs!4th単独公演 4ever〜ぼむずと紡ぐ∞の物語〜にて。オフィシャルカメラマンとして参加。 \n目のハイライトの入り方、好きです。",
@@ -202,7 +202,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo21",
-		lowResUrl: "_DSF0766-強化-NR_400.webp",
+		lowResUrl: "_DSF0766-強化-NR_1600.webp",
 		highResUrl: "_DSF0766-強化-NR_3200.jpg",
 		title: "ぼくらのうた",
 		comment: "Bombs!4th単独公演 4ever〜ぼむずと紡ぐ∞の物語〜にて。オフィシャルカメラマンとして参加。 \n卒業代の先輩方を撮影させていただきました。",
@@ -210,7 +210,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo22",
-		lowResUrl: "IMG_6408_400.webp",
+		lowResUrl: "IMG_6408_1600.webp",
 		highResUrl: "IMG_6408_3200.jpg",
 		title: "お見送り",
 		comment: "高校卒業後、島を出ていく友人のお見送り。",
@@ -218,7 +218,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo23",
-		lowResUrl: "IMG_2991_400.webp",
+		lowResUrl: "IMG_2991_1600.webp",
 		highResUrl: "IMG_2991_3200.jpg",
 		title: "筑波山",
 		comment: "筑波山の朝日峠から、日の出を撮影。",
@@ -226,7 +226,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo24",
-		lowResUrl: "IMG_2173_400.webp",
+		lowResUrl: "IMG_2173_1600.webp",
 		highResUrl: "IMG_2173_3200.jpg",
 		title: "高校、放課後",
 		comment: "下でラグビー部だったかが騒いでいたのをのぞいている図。\n めちゃ恋人風に見えますよね、そんなことないです。",
@@ -234,7 +234,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo25",
-		lowResUrl: "_DSF0958_400.webp",
+		lowResUrl: "_DSF0958_1600.webp",
 		highResUrl: "_DSF0958_3200.jpg",
 		title: "Sing Out!",
 		comment: "UNIDOL 2024 Summer 決勝戦にて。準優勝 明治大学・chocolat lumière\n オフィシャルカメラマンとして参加。",
@@ -242,7 +242,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo26",
-		lowResUrl: "_DSF0869-Enhanced-NR_400.webp",
+		lowResUrl: "_DSF0869-Enhanced-NR_1600.webp",
 		highResUrl: "_DSF0869-Enhanced-NR_3200.jpg",
 		title: "Panorama",
 		comment: "Bombs!3rd単独「Blooming 〜Bombs! 5th Anniversary〜」にて。オフィシャルカメラマンとして参加。",
@@ -250,7 +250,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo27",
-		lowResUrl: "_DSF1599_400.webp",
+		lowResUrl: "_DSF1599_1600.webp",
 		highResUrl: "_DSF1599_3200.jpg",
 		title: "秋",
 		comment: "地元ではこんな綺麗に紅葉しない。たまたまいた通行人がすごくよく写ってくれた一枚。",
@@ -258,7 +258,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo28",
-		lowResUrl: "_DSF1639_400.webp",
+		lowResUrl: "_DSF1639_1600.webp",
 		highResUrl: "_DSF1639_3200.jpg",
 		title: "銀杏",
 		comment: "光盛れしすぎている銀杏の葉。",
@@ -266,7 +266,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo29",
-		lowResUrl: "_DSF2079_400.webp",
+		lowResUrl: "_DSF2079_1600.webp",
 		highResUrl: "_DSF2079_3200.jpg",
 		title: "3A501",
 		comment: "学内を探検していた時に発見した教室。",
@@ -274,7 +274,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo30",
-		lowResUrl: "_DSF2889_400.webp",
+		lowResUrl: "_DSF2889_1600.webp",
 		highResUrl: "_DSF2889_3200.jpg",
 		title: "ひなたうた",
 		comment: "Doo-Wop夏ライブ2024にて。\n オフィシャルカメラマンとして参加。\n ナナニッパのデビュー戦でした。",
@@ -282,7 +282,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo31",
-		lowResUrl: "_DSF7342_400.webp",
+		lowResUrl: "_DSF7342_1600.webp",
 		highResUrl: "_DSF7342_3200.jpg",
 		title: "桜",
 		comment: "緋寒桜以外の桜って珍しい。地元にないし。",
@@ -290,7 +290,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo32",
-		lowResUrl: "_DSF2370_400.webp",
+		lowResUrl: "_DSF2370_1600.webp",
 		highResUrl: "_DSF2370_3200.jpg",
 		title: "Spring day",
 		comment: "コグマ 2025 卒ライ「約束」にて。\n オフィシャルカメラマンとして参加。\n",
@@ -298,7 +298,7 @@ export const photos: PhotoMeta[] = addPrefixes([
 	},
 	{
 		id: "photo33",
-		lowResUrl: "Z91_0301_400.webp",
+		lowResUrl: "Z91_0301_1600.webp",
 		highResUrl: "Z91_0301_3200.jpg",
 		title: "Bad girl Good girl",
 		comment: "コグマ 2025 卒ライ「約束」にて。\n オフィシャルカメラマンとして参加。\n",
